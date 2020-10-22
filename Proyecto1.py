172c3df9bba020aea75bae75fe08a1fb2da8fee0
Xothe <luis.fdz.mdz90@gmail.com>
2020-10-22 01:00:55 -0500

    Primer Proyecto en Python, imprimir en pantalla var

---
+++ Proyecto1.py
@@ -0,0 +1,16 @@
+mensaje1 = "Hola Mundo."
+dia_text = "El dia es"
+mes_text =  "del mes"
+ano_text = "del año"
+space = " "
+dia = 13
+mes = 10
+ano = 2020
+dia = str (dia)
+mes = str (mes)
+ano = str (ano)
+
+
+print  (mensaje1 + space + dia_text + space + dia + space + mes_text + space + mes + space + ano_text + space + ano ) 
+
+print (Mi primer Proyecto en python)
